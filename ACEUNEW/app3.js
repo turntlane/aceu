6b00ee3e425148acc58cdff15b11b19ef94d956d
turntlane <83042036+turntlane@users.noreply.github.com>
2021-06-10 16:49:03 -0700

    Add files via upload

---
+++ ACEUNEW/app3.js
@@ -0,0 +1,12 @@
+const text = baffle('.data2');
+const mouseWrap = baffle('.data2');
+const mouseHover = false;
+
+
+
+text.set({
+    characters : '>▒▓ /▓█░█ █▒█>░ ▒/▓ █▓>▒▒ ▓▓▒░ >█▒ ▓█▓< ▒/░▒',
+    speed: 100
+});
+text.start();
+text.reveal(4000);
