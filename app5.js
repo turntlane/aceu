30e8b0666801bcb671bf3a5a0e296cc847f1c69b
turntlane <83042036+turntlane@users.noreply.github.com>
2021-07-02 14:15:11 -0700

    Add files via upload
    hamburger menus

---
+++ app5.js
@@ -0,0 +1,11 @@
+const hamburger2 = document.getElementById('hamburger2');
+const navLinks2 = document.getElementById('nav-links2');
+
+
+hamburger2.addEventListener('click', () => {
+    navLinks2.classList.toggle('show')
+    
+    
+    navLinks2.style.zIndex = '1000000000';
+});
+
